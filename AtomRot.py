b3da5a3a96012b30d3d5df76a3e8a6c566ddfe03
Enrique Rodríguez <oenriquerdzlpz@gmail.com>
2024-02-13 18:23:00 -0600

    Código para rotar y trasladar

--- AtomRot.py
+++ AtomRot.py
@@ -2,79 +2,261 @@ import os
 import sys
 import math
 
-argumentList = sys.argv[1:]
+# Código hecho para mi buen amigo Rodrigo Aguilera
+# Con este código podrás rotar y trasladar átomos sin ningún problema
+#
+# Hecho por Otilio Enrique Rodríguez López
+# Instituto de Física, UASLP.
+# San Luis Potosí, México.
 
-#for number in range(1, 4):
-    #argumentList[number] = float(argumentList[number])
-    #print(argumentList[number])
-if argumentList:
-    print
-else:
-    print("-----------------------------------------------------------------------")
-    print(" ~ Valid input format should be: FileName.input.txt Alpha Beta Gamma ~ ")
-    print("-----------------------------------------------------------------------")
-    sys.exit()
+print("======================\
+      Bienvenido a AtomRoT\
+      =======================")
 
-alpha = float(argumentList[1])
-beta = float(argumentList[2])
-gamma = float(argumentList[3])
+print("")
 
-fileList = []
+rot_flag = False
+tras_flag = False
 
-if argumentList[0] == 'all':
-    print("All files will be rotated!")
-    for file in os.listdir('.'):
-        if file.endswith(".out.txt") or file.endswith(".input.txt"):
-            fileList.append(file)
+flag = True
+
+while flag:
+    what_to_do = input("¿Qué deseas realizar: Rotación o Traslación?(" + "\033[1m" + "r" + "\033[0m" + "ot/"\
+                       "\033[1m" + "t" + "\033[0m" + "as): ")
     
-    if fileList:
-        print("Found files:")
-        for files in fileList:
-            print(files)
-        print("")
+    if what_to_do.lower() == 'rot' or what_to_do.lower() == 'r'\
+        or what_to_do.lower() == 'rotacion' or what_to_do.lower() == 'rotación':
+        print("Se realizará una rotación.")
+        print("Ingrese los ángulos de rotación en grados.")
+
+        alpha_ = input("Alfa: ")
+        beta_ = input("Beta: ")
+        gamma_ = input("Gama: ")
+
+        print("Los ángulos de rotación son Alfa:", alpha_,", Beta:", beta_,", Gama: ", gamma_)
+        alpha = math.radians(float(alpha_))
+        beta = math.radians(float(beta_))
+        gamma = math.radians(float(gamma_))
+        flag = False
+        rot_flag = True
+
+    elif what_to_do.lower() == 'tras' or what_to_do.lower() == 't'\
+        or what_to_do.lower() == 'traslacion' or what_to_do.lower() == 'traslación':
+        print("Se realizará una traslación")
+        print("Ingrese los deltas(\u0394) para realizar la traslación.")
+        dx = input("\u0394x: ")
+        dy = input("\u0394y: ")
+        dz = input("\u0394z: ")
+        
+        print("Los deltas son \u0394x:", dx,", \u0394y:", dy,", \u0394z: ", dz)
+        dx = float(dx)
+        dy = float(dy)
+        dz = float(dz)
+
+        flag = False
+        tras_flag = True
+
     else:
-        print("No files were found!")
-        sys.exit()
+        print("Respuesta inválida")
 
-else:
-    print(argumentList[0], "file will be rotated")
-    fileList = [argumentList[0]]
 
-print("Rotational Angles are Alpha:", alpha,", Beta:", beta,", Gamma: ", gamma)
-alpha = math.radians(alpha)
-beta = math.radians(beta)
-gamma = math.radians(gamma)
+# while flag:
 
-print("--------------------------------")
-print(" ~ ~ Starting Atom Rotation ~ ~ ")
-print("--------------------------------")
+#     rot_input = input("¿Deseas realizar una rotación? (sí/no): ")
 
-for files in fileList:
-    file = open(files, 'r')
-    lines = file.readlines()
+#     if rot_input.lower() == 'si' or rot_input.lower() == 's'\
+#         or rot_input.lower() == 'yes' or rot_input.lower() == 'y':
+        
+#         print("Ingrese los ángulos de rotación en grados.")
+#         alpha_ = input("Alfa: ")
+#         beta_ = input("Beta: ")
+#         gamma_ = input("Gama: ")
+        
+#         print("Los ángulos de rotación son Alfa:", alpha_,", Beta:", beta_,", Gama: ", gamma_)
+#         alpha = math.radians(float(alpha_))
+#         beta = math.radians(float(beta_))
+#         gamma = math.radians(float(gamma_))
+#         flag = False
 
-    print("======>", files)
+#     elif rot_input.lower() == 'no' or rot_input.lower() == 'n':
+#         print('¡Ninguna rotación será efectuada!')
+#         flag = False
+#         rot_flag = False
 
-    f = open("rot_" + files, 'w')
-    f.write(str(lines[0].split()[0]) + '\n')
+#     else:
+#         print("Respuesta inválida")
 
-    for line in lines[2:]:
-        dataLine = line.split()
-        f.write("\n" + f"{dataLine[0] : <5}")
-        x = float(dataLine[1])
-        y = float(dataLine[2])
-        z = float(dataLine[3])
+
+
+# while flag and rot_flag != True:
+        
+#     tras_input = input("Deseas realizar una traslación? (sí/no): ")
+
+#     if tras_input.lower() == 'si' or tras_input.lower() == 's'\
+#         or tras_input.lower() == 'yes' or tras_input.lower() == 'y':
+        
+#         print("Ingrese los deltas(\u0394) para realizar la traslación.")
+#         dx = input("\u0394x: ")
+#         dy = input("\u0394y: ")
+#         dz = input("\u0394z: ")
+        
+#         print("Los deltas son \u0394x:", dx,", \u0394y:", dy,", \u0394z: ", dz)
+#         dx = float(dx)
+#         dy = float(dy)
+#         dz = float(dz)
+
+#         flag = False
+
+#     elif rot_input.lower() == 'no' or rot_input.lower() == 'n':
+
+#         print('¡Ninguna traslación será efectuada!')
+#         flag = False
+#         tras_flag = False
+
+#     else:
+#         print("Respuesta inválida")
+
+
+flag = True
+
+while flag and tras_flag == True:
+    fgen_input = input("¿Deseas generar más de un(1) archivo? (sí/no): ")
+
+    if fgen_input.lower() == 'si' or fgen_input.lower() == 's'\
+        or fgen_input.lower() == 'yes' or fgen_input.lower() == 'y':
+    
+        fnumber = input("Ingrese el número de archivos: ")
+        
+        print("Serán generados ", fnumber, " archivos")
+        flag = False
+        fnumber = int(fnumber)
+        if fnumber > 50:
+            confirmation_input = input("¿Está seguro? (sí/no)")
+            if confirmation_input.lower() != 'si' or confirmation_input.lower() != 's'\
+            or confirmation_input.lower() != 'yes' or confirmation_input.lower() != 'y':
+                flag = True
+        
+    elif fgen_input.lower() == 'no' or fgen_input.lower() == 'n':
+
+        print('Sólo un (1) archivo será generado.')
+        fnumber = 1
+        flag = False
+
+    else:
+        print("Respuesta inválida")
+
+if tras_flag == False and rot_flag == False:
+    print("No hay nada que hacer.")
+    print("Hasta luego.")
+    exit()
+
+#Leyendo archivo .vasp
+file = open("POSCAR.vasp", "r")
+lines = file.readlines()
+
+if rot_flag:
+    print("")
+    print("--------------------------------")
+    print(" ~ ~ Iniciando Rotación ~ ~ ")
+    print("--------------------------------")
+    print("")
+    #Creando archivo nuevo
+    new_file = open("POSCAR-rot-" +  str(alpha_) + "-" +  str(beta_) + "-" +  str(gamma_) + ".vasp", "w")
+    #Escribiendo las primeras líneas
+    new_file.write(lines[0])
+    new_file.write(lines[1])
+
+    #Iniciando rotación de los vectores base
+    for i in range(3):
+        x = float(lines[2 + i].split()[0])
+        y = float(lines[2 + i].split()[1])
+        z = float(lines[2 + i].split()[2])
 
         x_rot = x * math.cos(alpha) * math.cos(beta) + y * (math.cos(alpha) * math.sin(beta) * math.sin(gamma) - math.sin(alpha) * math.cos(gamma)) + z *(math.cos(alpha) * math.sin(beta) * math.cos(gamma) + math.sin(alpha) * math.sin(gamma))
         y_rot = x * math.sin(alpha) * math.cos(beta) + y * (math.sin(alpha) * math.sin(beta) * math.sin(gamma) + math.cos(alpha) * math.cos(gamma)) + z *(math.sin(alpha) * math.sin(beta) * math.sin(gamma) - math.cos(alpha) * math.sin(gamma))
         z_rot = -x * math.sin(beta) + y * math.cos(beta) * math.sin(gamma) + z * math.cos(beta) * math.cos(gamma)
         
-        x_rot = round(x_rot, 6)
-        y_rot = round(y_rot, 6)
-        z_rot = round(z_rot, 6)
+        x_rot = round(x_rot, 10)
+        y_rot = round(y_rot, 10)
+        z_rot = round(z_rot, 10)
 
-        f.write(f"{x_rot : ^20}{y_rot : ^20}{z_rot : ^20}")
+        new_file.write(f"{x_rot : 20.10f}{y_rot : 20.10f}{z_rot : 20.10f}" + "\n")
     
-    f.close()
+    new_file.write(lines[5])
+    new_file.write(lines[6])
     
-print("~ Done ~")
+    elements = lines[6].split()
+    total_cartesian = 0
+
+    for i in range(len(elements)):
+        total_cartesian += int(elements[i])
+   
+    new_file.write(lines[7])
+
+    for i in range(total_cartesian):
+        x = float(lines[8 + i].split()[0])
+        y = float(lines[8 + i].split()[1])
+        z = float(lines[8 + i].split()[2])
+
+        x_rot = x * math.cos(alpha) * math.cos(beta) + y * (math.cos(alpha) * math.sin(beta) * math.sin(gamma) - math.sin(alpha) * math.cos(gamma)) + z *(math.cos(alpha) * math.sin(beta) * math.cos(gamma) + math.sin(alpha) * math.sin(gamma))
+        y_rot = x * math.sin(alpha) * math.cos(beta) + y * (math.sin(alpha) * math.sin(beta) * math.sin(gamma) + math.cos(alpha) * math.cos(gamma)) + z *(math.sin(alpha) * math.sin(beta) * math.sin(gamma) - math.cos(alpha) * math.sin(gamma))
+        z_rot = -x * math.sin(beta) + y * math.cos(beta) * math.sin(gamma) + z * math.cos(beta) * math.cos(gamma)
+        
+        x_rot = round(x_rot, 10)
+        y_rot = round(y_rot, 10)
+        z_rot = round(z_rot, 10)
+
+        if i != (total_cartesian - 1):
+            new_file.write(f"{x_rot : 20.10f}{y_rot : 20.10f}{z_rot : 20.10f}" + "\n")
+        else:
+            new_file.write(f"{x_rot : 20.10f}{y_rot : 20.10f}{z_rot : 20.10f}")
+
+    new_file.close()
+
+elif tras_flag:
+    print("")
+    print("--------------------------------")
+    print(" ~ ~ Iniciando Traslación ~ ~ ")
+    print("--------------------------------")
+    print("")
+
+    #Creando múltiples archivos
+    for n in range(fnumber):
+        N = n + 1
+        new_file = open("POSCAR-tras-" +  str(round(N * dx, 2)) + "-" +  str(round(N * dy, 2)) + "-" +  str(round(N * dz, 2)) + ".vasp", "w")
+        #Escribiendo las primeras líneas
+        new_file.write(lines[0])
+        new_file.write(lines[1])
+        new_file.write(lines[2])
+        new_file.write(lines[3])
+        new_file.write(lines[4])
+        new_file.write(lines[5])
+        new_file.write(lines[6])
+
+        elements = lines[6].split()
+        total_cartesian = 0
+
+        for i in range(len(elements)):
+            total_cartesian += int(elements[i])
+    
+        new_file.write(lines[7])
+
+        for i in range(total_cartesian):
+            x = float(lines[8 + i].split()[0])
+            y = float(lines[8 + i].split()[1])
+            z = float(lines[8 + i].split()[2])
+
+            x_tras = round(x + N * dx, 10)
+            y_tras = round(y + N * dy, 10)
+            z_tras = round(z + N * dz, 10)
+
+            if i != (total_cartesian - 1):
+                new_file.write(f"{x_tras : 20.10f}{y_tras : 20.10f}{z_tras : 20.10f}" + "\n")
+            else:
+                new_file.write(f"{x_tras : 20.10f}{y_tras : 20.10f}{z_tras : 20.10f}")
+
+        new_file.close()
+
+print("--------------------------------")
+print(" ~ ~ Terminado ~ ~ ")
+print("--------------------------------")
